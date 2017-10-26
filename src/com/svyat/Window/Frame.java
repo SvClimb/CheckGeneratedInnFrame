fa40e818981ef454f14b353b84925aafa4d5416d
Svyatoslav <svclimb@gmail.com>
2017-10-26 17:22:37 +0300

    класс слушателей сделан внутренним классом Frame

--- src/com/svyat/Window/Frame.java
+++ src/com/svyat/Window/Frame.java
@@ -3,6 +3,8 @@ package com.svyat.Window;
 import com.svyat.InnEngine.InnGenerator;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by svcli on 24.10.2017.
@@ -73,7 +75,6 @@ public class Frame {
         frame.setContentPane(windowContent);
 
         // Наконец, отображаем окно
-      //  frame.setSize(400,200);
         frame.setBounds(760, 390, 400, 200);
         frame.setResizable(false);
         frame.setVisible(true);
@@ -83,4 +84,15 @@ public class Frame {
         GeneratorEngine generatorEngine = new GeneratorEngine();
         button_gener.addActionListener(generatorEngine);
     }
+
+    private class GeneratorEngine implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int setInt = Integer.valueOf((String)comboBox.getSelectedItem());
+            InnGenerator inn_gen = new InnGenerator(setInt);
+            if (setInt == 10) Frame.field_gener_inn.setText(String.valueOf(inn_gen.getInn10()));
+            else if(setInt == 12) Frame.field_gener_inn.setText(String.valueOf(inn_gen.getInn12()));
+        }
+    }
+
 }
